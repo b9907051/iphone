a1a6829f60844ca8738517d992ba075ae93ef1de
layx <lay9412206@gmail.com>
2019-10-09 16:00:23 +0800

    20191009

--- sql_Tmall.py
+++ sql_Tmall.py
@@ -1,31 +1,12 @@
 from bs4 import BeautifulSoup as Bt4
 import requests
-from flask import Flask, render_template, flash, redirect, url_for, session, request, logging
-from wtforms import Form, StringField, TextAreaField, PasswordField, validators
-from functools import wraps
-import requests
 import json
 import datetime
 import pandas as pd
-import os
-import shutil
-import pymysql.cursors
-from sshtunnel import SSHTunnelForwarder
-from os.path import expanduser
-import paramiko
-from paramiko import SSHClient
 import urllib
 import time
-import requests
 import re
-import heapq
 import random
-import platform
-
-from bs4 import BeautifulSoup
-import requests
-import random
-
 
 headers = {
 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
@@ -42,14 +23,14 @@ headers = {
 # 這個函式所有商品都可以用
 def Get_the_dealnumber(htmlscript,shopname):
 
-	shop_list = 'span[data-nick="%s"]'% shopname
+	shop_list = f'span[data-nick={shopname}]'
 	find_shop_span = htmlscript.select(shop_list)
 
 	try:
 		get_dealnumber_with_chinese = find_shop_span[0].parent.find('em').text
 		# 處理中文
 		# 如果拿到的字理面有萬，把後面兩個字"萬筆"去掉
-		if re.search("萬",get_dealnumber_with_chinese):
+		if re.search("萬", get_dealnumber_with_chinese):
 			find_deal_number = float(get_dealnumber_with_chinese[:-2])*10000
 		# 其餘狀況把筆去掉
 		else:
@@ -74,151 +55,99 @@ source = requests.get(url,headers = headers).text
 soup = Bt4(source,'html.parser')
 # print(soup)
 ban_by_Tmall = soup.find("p").text
-while True:
-	# 如果IP被Tmall ban掉的話
-	if '二正忙' in ban_by_Tmall:
-		print("目前被ban了")
-		time.sleep(3600)
-		continue
-	else:
+
+# 如果IP被Tmall ban掉的話
+if '二正忙' in ban_by_Tmall:
+	print("目前被ban了")
 	
-		shop_official = ['小米官方旗艦店','oppo官方旗艦店','華為官方旗艦店','vivo官方旗艦店']
-		shop_two_name = ['蘇寧易購官方旗艦店','三際數碼官方旗艦店']
+else:
 
-		#搜尋p30 = 搜尋p30+pro
-		product_by_url = {'小米9':'%s' % chinese_to_url('小米9'),'Opporeno':'oppo+reno',
-							'Vivo-X27':'vivo+x27','華為P30':'p30','華為P30Pro':'p30'} 
+	shop_official = ['小米官方旗艦店','oppo官方旗艦店','華為官方旗艦店','vivo官方旗艦店']
+	shop_two_name = ['蘇寧易購官方旗艦店','三際數碼官方旗艦店']
 
-		products_by_shops = {'小米9':[shop_official[0]],'Opporeno':[shop_official[1]],
-							'Vivo-X27':[shop_official[3]],'華為P30':[shop_official[2]],
-							'華為P30Pro':[shop_official[2]]}
+	#搜尋p30 = 搜尋p30+pro
+	product_by_url = {'小米9':'%s' % chinese_to_url('小米9'),'Opporeno':'oppo+reno',
+						'Vivo-X27':'vivo+x27','華為P30':'p30','華為P30Pro':'p30'} 
 
-		# 把非官方的網站跟官方的網站合起來,這裡 sum(欲合併的陣列,[])會把所有的陣列攤平合在一起,並做 flatten array
-		# 這裡稍微注意華為官方網站 的商品有兩個
-		for key in products_by_shops.keys():
-			products_by_shops[key] = sum([products_by_shops[key],shop_two_name],[])
+	products_by_shops = {'小米9':[shop_official[0]],'Opporeno':[shop_official[1]],
+						'Vivo-X27':[shop_official[3]],'華為P30':[shop_official[2]],
+						'華為P30Pro':[shop_official[2]]}
 
-		res = []
+	# 把非官方的網站跟官方的網站合起來,這裡 sum(欲合併的陣列,[])會把所有的陣列攤平合在一起,並做 flatten array
+	# 這裡稍微注意華為官方網站 的商品有兩個
+	for key in products_by_shops.keys():
+		products_by_shops[key] = sum([products_by_shops[key],shop_two_name],[])
 
-		#迴圈跑每個產品 - 如何華為的只跑一個??
-		for key in products_by_shops.keys():
+	res = []
 
-			# 如果產品是華為p30 直接跳過跑下一趟迴圈 只跑P30-Pro的商品
-			if key == '華為P30':
-				continue
+	#迴圈跑每個產品 - 如何華為的只跑一個??
+	for key in products_by_shops.keys():
 
-			url = 'https://list.tmall.com/search_product.htm?q=%s&sort=d' % product_by_url[key]
-			source = requests.get(url,headers = headers).text
-			soup = Bt4(source,'html.parser')
+		# 如果產品是華為p30 直接跳過跑下一趟迴圈 只跑P30-Pro的商品
+		if key == '華為P30':
+			continue
+
+		url = 'https://list.tmall.com/search_product.htm?q=%s&sort=d' % product_by_url[key]
+		source = requests.get(url,headers = headers).text
+		soup = Bt4(source,'html.parser')
 
 
 
-			#迴圈跑所有的店名
-			for shopname in products_by_shops[key]:
-				d = {}
+		#迴圈跑所有的店名
+		for shopname in products_by_shops[key]:
+			d = {}
 
-				#拿到 span標籤裡 data-nick 裡的文字是商店名
-				if key == '華為P30Pro':
-					p30_list = []
-					p30_Pro_list = []
+			#拿到 span標籤裡 data-nick 裡的文字是商店名
+			if key == '華為P30Pro':
+				p30_list = []
+				p30_Pro_list = []
 
-					find_a_label_for_huawei = soup.find_all("a",title=re.compile(r'p30',re.I))
-					regular_expression_pro = re.compile(r'pro',re.I)
+				find_a_label_for_huawei = soup.find_all("a",title=re.compile(r'p30',re.I))
+				regular_expression_pro = re.compile(r'pro',re.I)
 
-					for index, item in enumerate(find_a_label_for_huawei):
-						# 如果找不到Pro 就表示產品為 p30
-						
-						if not regular_expression_pro.search(find_a_label_for_huawei[index]['title']):
-							p30_list.append(item.parent.parent)
-						# 否則該產品為p30 Pro
-
-						else:
-							p30_Pro_list.append(item.parent.parent)
-					# 因為這裡不知道到底 產品 [key] 是 華為p30還是華為p30-Pro 我這裡用 try catch處理
-					# 若 key 跑到 p30 的時候 沒有問題 
-
-					# 
-					for htmlscript_in_list in p30_list:
-						# 抓下來的資料處理完以後的商店名字跟現在要處理的商店名字一樣實跳出迴圈
-						if Get_the_dealnumber(htmlscript_in_list,shopname)[0] == shopname:
-							d['Dealnumber'] = Get_the_dealnumber(htmlscript_in_list,shopname)[1]
-							d['Shopname'] = shopname
-							d['Product'] = '華為P30'
-							d['Timestamp'] = datetime.datetime.today().strftime("%Y-%m-%d")
-							res.append(d)
-							# 這裡若 d 不做清空接著跑P30Pro的時候新寫入的資料會把P30給蓋掉
-							d = {}
-							break
+				for index, item in enumerate(find_a_label_for_huawei):
+					# 如果找不到Pro 就表示產品為 p30
 					
-					for htmlscript_in_list in p30_Pro_list:
-						if Get_the_dealnumber(htmlscript_in_list,shopname)[0] == shopname:
-							d['Dealnumber'] = Get_the_dealnumber(htmlscript_in_list,shopname)[1]
-							d['Shopname'] = shopname
-							d['Product'] = '華為P30Pro'
-							d['Timestamp'] = datetime.datetime.today().strftime("%Y-%m-%d")
-							res.append(d)
-							d = {}
-							break
+					if not regular_expression_pro.search(find_a_label_for_huawei[index]['title']):
+						p30_list.append(item.parent.parent)
+					# 否則該產品為p30 Pro
 
-					
-				# 如果不是華為的產品
-				else:
+					else:
+						p30_Pro_list.append(item.parent.parent)
+				# 因為這裡不知道到底 產品 [key] 是 華為p30還是華為p30-Pro 我這裡用 try catch處理
+				# 若 key 跑到 p30 的時候 沒有問題 
 
-					time.sleep(random.randint(1,3))
-					d['Shopname'] = shopname
-					d['Product'] = key
-					d['Timestamp'] = datetime.datetime.today().strftime("%Y-%m-%d")
-					d['Dealnumber'] = Get_the_dealnumber(soup,shopname)[1]
-					res.append(d)
+				# 
+				for htmlscript_in_list in p30_list:
+					# 抓下來的資料處理完以後的商店名字跟現在要處理的商店名字一樣實跳出迴圈
+					if Get_the_dealnumber(htmlscript_in_list,shopname)[0] == shopname:
+						d['Dealnumber'] = Get_the_dealnumber(htmlscript_in_list,shopname)[1]
+						d['Shopname'] = shopname
+						d['Product'] = '華為P30'
+						d['Timestamp'] = datetime.datetime.today().strftime("%Y-%m-%d")
+						res.append(d)
+						# 這裡若 d 不做清空接著跑P30Pro的時候新寫入的資料會把P30給蓋掉
+						d = {}
+						break
+				
+				for htmlscript_in_list in p30_Pro_list:
+					if Get_the_dealnumber(htmlscript_in_list,shopname)[0] == shopname:
+						d['Dealnumber'] = Get_the_dealnumber(htmlscript_in_list,shopname)[1]
+						d['Shopname'] = shopname
+						d['Product'] = '華為P30Pro'
+						d['Timestamp'] = datetime.datetime.today().strftime("%Y-%m-%d")
+						res.append(d)
+						d = {}
+						break
 
-		# return render_template('index.html')
-		# print(res)
-		sql_hostname = 'mytestdb.c72ftrj7ifc7.ap-northeast-1.rds.amazonaws.com'
-		sql_username = 'root'
-		sql_password = 'lay911225'
-		sql_main_database = 'Appleinfo'
-		sql_port = 3306
-		ssh_host = 'ec2-13-114-122-68.ap-northeast-1.compute.amazonaws.com'
-		ssh_user = 'ec2-user'
-		ssh_port = 22
-		sql_ip = '1.1.1.1.1'
+				
+			# 如果不是華為的產品
+			else:
 
-		if platform.system() == 'Windows':
-			#Local 端
-			mypkey = paramiko.RSAKey.from_private_key_file('C:\\Users\\user\\Desktop\\Layx_Tokyo.pem')
-		else:
-			# AWS 端
-			mypkey = paramiko.RSAKey.from_private_key_file('/home/ec2-user/Chinasmartphone/Layx_Tokyo.pem')
+				time.sleep(random.randint(1,3))
+				d['Shopname'] = shopname
+				d['Product'] = key
+				d['Timestamp'] = datetime.datetime.today().strftime("%Y-%m-%d")
+				d['Dealnumber'] = Get_the_dealnumber(soup,shopname)[1]
+				res.append(d)
 
-		with SSHTunnelForwarder(
-			(ssh_host, ssh_port),
-			ssh_username=ssh_user,
-			ssh_pkey=mypkey,
-			remote_bind_address=(sql_hostname, sql_port)) as tunnel:
-			connection = pymysql.connect(host='127.0.0.1', user=sql_username,
-			passwd=sql_password, db=sql_main_database,
-			port=tunnel.local_bind_port)
-
-			# 寫入資料	
-			with connection.cursor() as cursor:
-				for c in res:
-
-					# '這個格式直接把'
-					sql = """INSERT INTO
-					 Tmall.Data 
-					 (Product,Timestamp,Shopname,Dealnumber) 
-					 VALUES (%(Product)s,%(Timestamp)s,%(Shopname)s,%(Dealnumber)s)"""
-
-					# 執行sql語法 輸入 users
-
-					# cursor.execute(sql, c)
-					# print(sql,c)
-					try:
-						cursor.execute(sql,c)
-						connection.commit()
-					except:
-						print(c)
-						print("錯誤發生有可能是引入相同的資料")
-						pass
-				print('資料寫入完成')
-		break
--- sql_Zhongguancun.py
+++ sql_Zhongguancun.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup as Bt4
 import requests
 from flask import Flask, render_template, flash, redirect, url_for, session, request, logging
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators
 from functools import wraps
 import requests
 import json
@@ -8,17 +9,27 @@ import datetime
 import pandas as pd
 import os
 import shutil
-import datetime
 import pymysql.cursors
 from sshtunnel import SSHTunnelForwarder
 from os.path import expanduser
 import paramiko
 from paramiko import SSHClient
+import urllib
+import time
+import requests
+import re
+import heapq
+import random
 import platform
+
+from bs4 import BeautifulSoup
+import requests
+import random
 
 product_index = {'Opporeno':['1262','1261155'],'小米9':['1225','1224655'],
 				'華為P30':['1213','1212233'],'華為P30Pro':['1224','1223829'],
-				'Vivo-X27':['1263','1262905']}
+				'Vivo-X27':['1263','1262905'],
+				'華為Mat30Pro-4G':['1292','1291827'],'華為Mat30Pro-5G':['1266','1265181']}
 
 #為了方便接下來 不同的 網頁index 進行 產品  的對應 將 key 跟 產品互換
 #舉例 product_index {1261155: 'Opporeno',...}
@@ -44,51 +55,51 @@ for key,index in product_index.items():
 
 # return render_template('index.html')
 # print(res)
-sql_hostname = 'mytestdb.c72ftrj7ifc7.ap-northeast-1.rds.amazonaws.com'
-sql_username = 'root'
-sql_password = 'lay911225'
-sql_main_database = 'Appleinfo'
-sql_port = 3306
-ssh_host = 'ec2-13-114-122-68.ap-northeast-1.compute.amazonaws.com'
-ssh_user = 'ec2-user'
-ssh_port = 22
-sql_ip = '1.1.1.1.1'
+# sql_hostname = 'mytestdb.c72ftrj7ifc7.ap-northeast-1.rds.amazonaws.com'
+# sql_username = 'root'
+# sql_password = 'lay911225'
+# sql_main_database = 'Appleinfo'
+# sql_port = 3306
+# ssh_host = 'ec2-13-114-122-68.ap-northeast-1.compute.amazonaws.com'
+# ssh_user = 'ec2-user'
+# ssh_port = 22
+# sql_ip = '1.1.1.1.1'
 
-if platform.system() == 'Windows':
-	#Local 端
-	mypkey = paramiko.RSAKey.from_private_key_file('C:\\Users\\user\\Desktop\\Layx_Tokyo.pem')
-else:
-	# AWS 端
-	mypkey = paramiko.RSAKey.from_private_key_file('/home/ec2-user/Chinasmartphone/Layx_Tokyo.pem')
+# if platform.system() == 'Windows':
+# 	#Local 端
+# 	mypkey = paramiko.RSAKey.from_private_key_file('C:\\Users\\user\\Desktop\\Layx_Tokyo.pem')
+# else:
+# 	# AWS 端
+# 	mypkey = paramiko.RSAKey.from_private_key_file('/home/ec2-user/Chinasmartphone/Layx_Tokyo.pem')
 
-with SSHTunnelForwarder(
-	(ssh_host, ssh_port),
-	ssh_username=ssh_user,
-	ssh_pkey=mypkey,
-	remote_bind_address=(sql_hostname, sql_port)) as tunnel:
-	connection = pymysql.connect(host='127.0.0.1', user=sql_username,
-	passwd=sql_password, db=sql_main_database,
-	port=tunnel.local_bind_port)
+# with SSHTunnelForwarder(
+# 	(ssh_host, ssh_port),
+# 	ssh_username=ssh_user,
+# 	ssh_pkey=mypkey,
+# 	remote_bind_address=(sql_hostname, sql_port)) as tunnel:
+# 	connection = pymysql.connect(host='127.0.0.1', user=sql_username,
+# 	passwd=sql_password, db=sql_main_database,
+# 	port=tunnel.local_bind_port)
 
-	# 寫入資料	
-	with connection.cursor() as cursor:
-		for c in res:
+# 	# 寫入資料	
+# 	with connection.cursor() as cursor:
+# 		for c in res:
 
-			# '這個格式直接把'
-			sql = """INSERT INTO
-			 China_Zhongguancun.Data 
-			 (Product,Timestamp,Price) 
-			 VALUES (%(Product)s,%(Timestamp)s,%(Price)s)"""
+# 			# '這個格式直接把'
+# 			sql = """INSERT INTO
+# 			 China_Zhongguancun.Data 
+# 			 (Product,Timestamp,Price) 
+# 			 VALUES (%(Product)s,%(Timestamp)s,%(Price)s)"""
 
-			# 執行sql語法 輸入 users
+# 			# 執行sql語法 輸入 users
 			
-			# cursor.execute(sql, c)
-			try:
-				cursor.execute(sql,c)
-				connection.commit()
+# 			# cursor.execute(sql, c)
+# 			try:
+# 				cursor.execute(sql,c)
+# 				connection.commit()
 
-			except:
-				print(c)
-				print("錯誤發生有可能是引入相同的資料")
-				pass
+# 			except:
+# 				print(c)
+# 				print("錯誤發生有可能是引入相同的資料")
+# 				pass
 
