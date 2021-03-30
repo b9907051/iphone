e5dc2c16efb4756bf3b9e5a02e3c006175292f9c
Layx <lay9412206@gmail.com>
2021-03-30 16:44:43 +0800

    update

--- Shipping.py
+++ Shipping.py
@@ -95,91 +95,92 @@ result.to_csv(path + output_direct +output_filename +'.csv',index = False,encodi
 
 # ----------  爬取 港口班輪班率  ----------
 
-website = f"https://www.sse.net.cn/index/singleIndex?indexType=gcspi_port"
-source = requests.get(website).text
-soup = Bt4(source, "lxml")
+# website = f"https://www.sse.net.cn/index/singleIndex?indexType=gcspi_port"
+# source = requests.get(website).text
+# soup = Bt4(source, "lxml")
 
-form = soup.find_all('script')[1].getText()
-# 把字串先做些前處理 把\' 換成 空的
-form = form.replace('\'',"")
+# form = soup.find_all('script')[1].getText()
+# print(form)
+# # 把字串先做些前處理 把\' 換成 空的
+# form = form.replace('\'',"")
 
-# 這裡把數字抓下來
-# 抓下來會是一個tupo 第一個為 row的index 第二個為資料
-# row[0]:港口名字, row[1]:本期準班率, row[3]:本期靠掛數, row[7]:在泊時間,row[8]:在港時間
-#需注意的是抓下來的數字第6個index的倍數是隱藏的資訊沒有放在網頁上
-search_rule = re.compile(r'''
-row\[(\d)\]\= # row[數字]=
-([^0-9.]+|[0-9.-]+);
-''',re.VERBOSE)
+# # 這裡把數字抓下來
+# # 抓下來會是一個tupo 第一個為 row的index 第二個為資料
+# # row[0]:港口名字, row[1]:本期準班率, row[3]:本期靠掛數, row[7]:在泊時間,row[8]:在港時間
+# #需注意的是抓下來的數字第6個index的倍數是隱藏的資訊沒有放在網頁上
+# search_rule = re.compile(r'''
+# row\[(\d)\]\= # row[數字]=
+# ([^0-9.]+|[0-9.-]+);
+# ''',re.VERBOSE)
 
-all_data = search_rule.findall(form)
+# all_data = search_rule.findall(form)
 
-result = []
-d = {}
-for i in range(1,len(all_data)):
-    if (all_data[i-1][0] == '0'):
-        d['港口'] = all_data[i-1][1]
+# result = []
+# d = {}
+# for i in range(1,len(all_data)):
+#     if (all_data[i-1][0] == '0'):
+#         d['港口'] = all_data[i-1][1]
         
-    elif (all_data[i-1][0] == '1'):
-        d['本期準班率'] = all_data[i-1][1]
+#     elif (all_data[i-1][0] == '1'):
+#         d['本期準班率'] = all_data[i-1][1]
 
-    elif (all_data[i-1][0] == '3'):
-        d['本期靠掛數'] = all_data[i-1][1]
+#     elif (all_data[i-1][0] == '3'):
+#         d['本期靠掛數'] = all_data[i-1][1]
 
-    elif (all_data[i-1][0] == '7'):
-        d['在泊時間'] = all_data[i-1][1]
+#     elif (all_data[i-1][0] == '7'):
+#         d['在泊時間'] = all_data[i-1][1]
 
-    elif (all_data[i-1][0] == '8'):
-        d['在港時間'] = all_data[i-1][1]
-#     迴圈每走9次就append一次 在index = 0的時候不算
-    if (i%9) == 0:
-        result.append(d)
-        d = {}
+#     elif (all_data[i-1][0] == '8'):
+#         d['在港時間'] = all_data[i-1][1]
+# #     迴圈每走9次就append一次 在index = 0的時候不算
+#     if (i%9) == 0:
+#         result.append(d)
+#         d = {}
 
-table = pd.DataFrame(result)
-# 只要幾個港口
-# table[上海纽约长滩  洛杉矶 奥克兰（美国） 鹿特丹 安特卫普
-# 只要幾個港口
-table = table[(table['港口'] == '上海') | (table['港口'] == '纽约') | (table['港口'] == '长滩') |
-      (table['港口'] == '洛杉矶') | (table['港口'] == '奥克兰（美国）') | (table['港口'] == '鹿特丹') | 
-      (table['港口'] == '安特卫普')]
-# table
-table.index = table['港口']
-table.drop(columns = '港口')
+# table = pd.DataFrame(result)
+# # 只要幾個港口
+# # table[上海纽约长滩  洛杉矶 奥克兰（美国） 鹿特丹 安特卫普
+# # 只要幾個港口
+# table = table[(table['港口'] == '上海') | (table['港口'] == '纽约') | (table['港口'] == '长滩') |
+#       (table['港口'] == '洛杉矶') | (table['港口'] == '奥克兰（美国）') | (table['港口'] == '鹿特丹') | 
+#       (table['港口'] == '安特卫普')]
+# # table
+# table.index = table['港口']
+# table.drop(columns = '港口')
 
-# 拿到資料的時間
-time = soup.find_all("div", {"class": "title2"})[0].get_text()[0:7]
-date = pd.DataFrame({'日期':[time]})
+# # 拿到資料的時間
+# time = soup.find_all("div", {"class": "title2"})[0].get_text()[0:7]
+# date = pd.DataFrame({'日期':[time]})
 
-result = pd.DataFrame()
-# 不知道為什麼已經把 港口drop掉了column還是會有港口 所以這裡我單獨拿出來處理
-columnlist = ['本期準班率','本期靠掛數','在港時間','在泊時間']
+# result = pd.DataFrame()
+# # 不知道為什麼已經把 港口drop掉了column還是會有港口 所以這裡我單獨拿出來處理
+# columnlist = ['本期準班率','本期靠掛數','在港時間','在泊時間']
 
 
-# 本期靠掛數
-for column in columnlist:
-    temp = table.loc[:,[column,'港口']]
-    temp = temp[[column]]
-    temp = temp.T
-    # 把 coulmnlist 前四個字加到
-    temp.columns = [i + column[0:4] for i in temp.columns]
-    temp.index = [time]
-    result = pd.concat([result,temp],axis = 1)
+# # 本期靠掛數
+# for column in columnlist:
+#     temp = table.loc[:,[column,'港口']]
+#     temp = temp[[column]]
+#     temp = temp.T
+#     # 把 coulmnlist 前四個字加到
+#     temp.columns = [i + column[0:4] for i in temp.columns]
+#     temp.index = [time]
+#     result = pd.concat([result,temp],axis = 1)
     
-# 把index改成 0 才能將 df 等下跟時間合併 
-result.index = [0]
-result = pd.concat([date,result],axis = 1)
+# # 把index改成 0 才能將 df 等下跟時間合併 
+# result.index = [0]
+# result = pd.concat([date,result],axis = 1)
 
-output_direct = 'Shipping/'
-output_filename = 'shipment2'
+# output_direct = 'Shipping/'
+# output_filename = 'shipment2'
 
     
-# 將新舊的資料合併
-old_data = pd.read_csv(path + output_direct +output_filename +'.csv',encoding='utf_8_sig')
-save_data = pd.concat([old_data,result])
-# 如果資料沒有更新就移除
-save_data.drop_duplicates(inplace = True)
-result.to_csv(path + output_direct +output_filename +'.csv',index = False,encoding='utf_8_sig')
+# # 將新舊的資料合併
+# old_data = pd.read_csv(path + output_direct +output_filename +'.csv',encoding='utf_8_sig')
+# save_data = pd.concat([old_data,result])
+# # 如果資料沒有更新就移除
+# save_data.drop_duplicates(inplace = True)
+# result.to_csv(path + output_direct +output_filename +'.csv',index = False,encoding='utf_8_sig')
 
 # ----------  爬取 主幹航線準班率  ----------
 
