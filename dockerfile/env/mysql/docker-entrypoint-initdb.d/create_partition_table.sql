31f882ae55ce44938b0f7959028935c67ea552d1
layx <layx@cathaylife.com.tw>
2022-07-15 15:51:19 +0800

    update

--- dockerfile/env/mysql/docker-entrypoint-initdb.d/create_partition_table.sql
+++ dockerfile/env/mysql/docker-entrypoint-initdb.d/create_partition_table.sql
@@ -20,15 +20,48 @@ CREATE TABLE `Laptop`.`HP`(
     PRIMARY KEY(`Oid`, `Date`)
 )
 
-CREATE DATABASE phone;
-CREATE TABLE `phone`.`iPhone`(
+CREATE TABLE `Laptop`.`Bestbuy`(
+    `lowprice_average` float(24) NOT NULL,
+    `highprice_average` float(24) NOT NULL,
+    `soldout_percent` float(24) NOT NULL,
+    `onsale_percent` float(24) NOT NULL,
+    `total_amount` SMALLINT unsigned NOT NULL,
+    `ordeble_amount` SMALLINT unsigned NOT NULL,
+    `soldout_amount` SMALLINT unsigned NOT NULL,
+    `onsale_amount` SMALLINT unsigned NOT NULL,
+    `non_onsale_amount` SMALLINT unsigned NOT NULL,
+    `onsale_dollar_percent` float(24) NOT NULL,
+    `timestamp` DATE NOT NULL,
+    PRIMARY KEY(`timestamp`)
+)
+
+CREATE DATABASE Phone;
+CREATE TABLE `Phone`.`iPhone`(
     `Country` VARCHAR(2) NOT NULL,
     `TimeStemp` DATE NOT NULL,
     `Product` VARCHAR(20) NOT NULL,
     `Colors` VARCHAR(10) NOT NULL,
     `Size` VARCHAR(5) NOT NULL,
-    `Deliver` VARCHAR(30) NOT NULL,
+    `Deliver` VARCHAR(50) NOT NULL,
     `Day` SMALLINT unsigned NOT NULL,
     `Celluar` VARCHAR(15),
-    PRIMARY KEY(`Country`,`Product`,`Colors`,`Size`,`Celluar`, `TimeStemp`)
+    PRIMARY KEY(`Country`,`Product`,`Colors`,`Size`,`Celluar`, `TimeStemp`,`Day`,`Deliver`)
+)
+
+CREATE DATABASE Sports;
+CREATE TABLE `Sports`.`Nike`(
+    `timestamp` DATE NOT NULL,
+    `discount_item_ratio_cloth` float(24) NOT NULL,
+    `discount_money_cloth` float(24) NOT NULL,
+    `total_num_cloth` SMALLINT unsigned NOT NULL,
+    `soldout_num_cloth` SMALLINT unsigned NOT NULL,
+    `instock_num_cloth` SMALLINT unsigned NOT NULL,
+    `discount_num_cloth` SMALLINT unsigned NOT NULL,
+    `discount_item_ratio_shoes` float(24) NOT NULL,
+    `discount_money_shoes` float(24) NOT NULL,
+    `total_num_shoes` SMALLINT unsigned NOT NULL,
+    `soldout_num_shoes` SMALLINT unsigned NOT NULL,
+    `instock_num_shoes` SMALLINT unsigned NOT NULL,
+    `discount_num_shoes` SMALLINT unsigned NOT NULL,
+    PRIMARY KEY(`timestamp`)
 )
